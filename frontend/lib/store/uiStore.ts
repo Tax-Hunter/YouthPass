74346669957ad977ee2624079567463d7f83847f
chodonghee-hub <timandsunny@naver.com>
2026-06-29 15:41:41 +0900

    feat: UI 모달 상태 스토어(uiStore) 및 useHydrated 훅 추가

---
+++ frontend/lib/store/uiStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand"
+
+interface UiState {
+  loginModalOpen: boolean
+  openLoginModal: () => void
+  closeLoginModal: () => void
+  supportModalOpen: boolean
+  openSupportModal: () => void
+  closeSupportModal: () => void
+}
+
+export const useUiStore = create<UiState>((set) => ({
+  loginModalOpen: false,
+  openLoginModal: () => set({ loginModalOpen: true }),
+  closeLoginModal: () => set({ loginModalOpen: false }),
+  supportModalOpen: false,
+  openSupportModal: () => set({ supportModalOpen: true }),
+  closeSupportModal: () => set({ supportModalOpen: false }),
+}))
