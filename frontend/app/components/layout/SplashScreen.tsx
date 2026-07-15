b7a8b451ab0ff895b09337d2526042cbc5ab76ae
chodonghee-hub <timandsunny@naver.com>
2026-07-15 12:24:30 +0900

    feat: 스플래시 스크린 컴포넌트 추가

---
+++ frontend/app/components/layout/SplashScreen.tsx
@@ -0,0 +1,22 @@
+interface Props {
+  visible: boolean;
+}
+
+export default function SplashScreen({ visible }: Props) {
+  return (
+    <div
+      id="app-splash"
+      className={`absolute inset-0 z-[70] flex items-center justify-center bg-white transition-opacity duration-500 ${
+        visible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/images/logo.png"
+        alt="청년패스 로고"
+        draggable={false}
+        className="w-40 h-auto object-contain select-none animate-fade-in"
+      />
+    </div>
+  );
+}
